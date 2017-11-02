e20ebe306b3c5dd75feca6dc88d8f43b133b3d7c
icestains <ourlichi@gmail.com>
2017-11-02 20:23:49 +0800

    git commit -m "update ele"

--- Elevator.cpp
+++ Elevator.cpp
@@ -5,11 +5,11 @@ int main() {
 	int m[105] = { 0 };
 	while (cin>> n&&n!=0) {
 		int shang = 0, xia = 0;
-		for (i = 1; i <= n; i++) cin >> m[i];//ÊäÈë 
+		for (i = 1; i <= n; i++) cin >> m[i];//ï¿½ï¿½ï¿½ï¿½ 
 		for (i = 0; i < n; i++) {
-			if ((m[i + 1] - m[i]) < 0)//ÏÂÂ¥ 
+			if ((m[i + 1] - m[i]) < 0)//ï¿½ï¿½Â¥ 
 				xia = xia + m[i + 1] - m[i];
-			else//ÉÏÂ¥ 
+			else//ï¿½ï¿½Â¥ 
 				shang = shang + m[i + 1] - m[i];
 		}
 		cout << n * 5 + shang * 6 - xia * 4 << endl;
@@ -17,9 +17,4 @@ int main() {
 	return 0;
 }
 
-//#include<fstream>
-	//ifstream file;
-	//file.open("input.txt");
-		//cout << n << " " << shang << " " << xia << endl;
-		
-	//file.close();
+
